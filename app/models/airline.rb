a113fa96c1eb22f19826b22867c36013440f2b9b
Tenzin Jorden <tenden96@gmail.com>
2022-09-22 22:54:15 -0400

    5th commit

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -9,7 +9,7 @@ class Airline < ApplicationRecord
     end
 
     def avg_score
-        # averages the score from the reviews model and rounds to 2 places and converts to float
+        # averages the score from the reviews model and rounds to 2 places and converts to floa
         reviews.average(:score).round(2).to_f
     end
 end
